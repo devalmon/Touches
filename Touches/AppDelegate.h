deead13321c9cca813b98a7919a565284ab5e324
devalmon <phlood@yandex.ru>
2020-05-30 14:02:32 +0300

    Initial Commit

---
+++ Touches/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Touches
+//
+//  Created by Alexey Baryshnikov on 30.05.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Touches/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Touches
+//
+//  Created by Alexey Baryshnikov on 30.05.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
